a069689aa069961e70f31346d96f89522a613ade
Yang Tian <v-yangtian@micosoft.com>
2022-07-12 22:56:54 +0800

    update

---
+++ ScopeApplication/LwNoduleText_Combine.script.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.SCOPE.Types;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using ScopeRuntime;
+class MyClass
+{
+    public static string GetLatestDate(string path)
+    {
+        if(string.IsNullOrEmpty(path)) return "";
+        var strs=path.Split('/');
+        if(strs.Length>8) return strs[8];
+        return "";
+    }
+
+}
